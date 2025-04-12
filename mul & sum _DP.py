1848c6d4bf8694cc661159aaeca87599c1cfe0aa
Divyansha Parulekar <parulekardivyansha@gmail.com>
2025-04-12 13:40:47 +0530

    Add files via upload

---
+++ mul & sum _DP.py
@@ -0,0 +1,12 @@
+#Pyton program to calculate the multiplication and sum of two numbers
+x = int(input("Enter first number: "))
+y = int(input("Enter Second number: "))
+mul = x*y
+print(f"The multiplication of {x} and {y} is {mul}")
+
+
+# sum of two numbers
+x = int(input("Enter sum of first number: "))
+y = int(input("Enter sum of second number: "))
+sum = x+y
+print(f"The sum of {x} and {y} is {sum}")
