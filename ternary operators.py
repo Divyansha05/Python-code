461512814bc267829ea1840c47d75ab3b56177fc
Divyansha Parulekar <parulekardivyansha@gmail.com>
2025-04-12 14:09:53 +0530

    Add files via upload

---
+++ ternary operators.py
@@ -0,0 +1,6 @@
+# declare two variables and print that which two variable is largest using ternay opertor
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+ans = num1 if num1>num2 else num2
+print(ans)
