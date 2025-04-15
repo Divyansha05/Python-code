9aac3db74a752217c000b094f8706be1cb46becc
Divyansha Parulekar <parulekardivyansha@gmail.com>
2025-04-15 16:26:28 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,6 @@
+# python program to find the area of a triangle whose sides are given
+
+b = int(input("Enter base of an triangle: "))
+h = int(input("Enter height of an triangle: "))
+area = (1/2*b*h)
+print(area)
