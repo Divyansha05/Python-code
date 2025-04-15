606b89d61f5ae36cf83fdd6a6f771319f0d5493d
Divyansha Parulekar <parulekardivyansha@gmail.com>
2025-04-15 16:25:53 +0530

    Add files via upload

---
+++ convert degree to farhenheit.py
@@ -0,0 +1,5 @@
+# Python program to covert the temperature in degree centigrade to fahrenheit
+
+a = int(input("Enter a degree to convert it into fahrenheit: "))
+fahrenheit = (a*9/5)+32
+print("This is the conversion: ", fahrenheit)
