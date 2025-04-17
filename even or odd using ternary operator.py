9312c02dc2ee97eea40a450ecf27e729dd2e9784
Divyansha Parulekar <parulekardivyansha@gmail.com>
2025-04-17 22:41:49 +0530

    Add files via upload

---
+++ even or odd using ternary operator.py
@@ -0,0 +1,10 @@
+# Write a python program using input() function take one number from the user and using ternary operators check whether the number is even or odd
+
+a = int(input("Enter any numbers:"))
+if a%2==0:
+    print("It's even number")   #ouput:Enter any numbers:4 It's even number
+else:
+    print("It's odd number")    #ouput:Enter any numbers:1 It's odd number
+
+
+
