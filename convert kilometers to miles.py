97a31be7a1c21b4802611d200d8fbd3be529241b
Divyansha Parulekar <parulekardivyansha@gmail.com>
2025-04-17 22:43:11 +0530

    Add files via upload

---
+++ convert kilometers to miles.py
@@ -0,0 +1,14 @@
+# Write a python program to convert Kilometers to Miles
+
+Kilometers = float(input("Enter value in Kilometers: "))
+
+conv_fac = 0.621371
+
+miles = Kilometers * conv_fac
+print("%0.2f Kilometers is equal to %0.2f miles" %(Kilometers,miles))
+
+
+
+#output:
+# Enter value in Kilometers: 2.5
+# 2.50 Kilometers is equal to 1.55 miles
